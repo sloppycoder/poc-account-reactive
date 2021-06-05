bc26caa592943171ac7690d6bedb4899f69fa7df
Li Lin <guru.lin@gmail.com>
2021-06-05 20:51:03 +0800

    generated from quarkus.io

---
+++ src/test/java/org/vino9/poc/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.vino9.poc;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
