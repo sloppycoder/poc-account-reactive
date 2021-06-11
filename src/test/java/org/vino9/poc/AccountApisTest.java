03f4b212d8ddab48a746daddf1e6d8b32e63f45b
Li Lin <guru.lin@gmail.com>
2021-06-11 15:40:36 +0800

    added health probes

--- src/test/java/org/vino9/poc/AccountApisTest.java
+++ src/test/java/org/vino9/poc/AccountApisTest.java
@@ -16,22 +16,46 @@ class AccountApisTest {
 
   @Test
   void swagger_ui_is_available() {
-    given().when().get("/swagger-ui").then().statusCode(200).body(containsString("Swagger"));
+    given()
+        .when()
+            .get("/swagger-ui")
+        .then()
+            .statusCode(200)
+            .body(containsString("Swagger"));
   }
 
   @Test
   void prometheus_metrics_is_available() {
-    given().when().get("/q/metrics").then().statusCode(200).body(containsString("jvm"));
+    given()
+        .when()
+            .get("/q/metrics")
+        .then()
+            .statusCode(200)
+            .body(containsString("jvm"));
   }
 
   @Test
   void legit_account_can_be_returned() {
-    given().when().get("/accounts/11223344").then().statusCode(200).body("currency", equalTo("SGD"));
+    given()
+        .when()
+            .get("/accounts/11223344").then().statusCode(200).body("currency", equalTo("SGD"));
   }
 
   @Test
   void unknown_account_returns_not_found() {
-    given().when().get("/accounts/8005551212").then().statusCode(404);
+    given()
+        .when()
+            .get("/accounts/8005551212")
+        .then()
+            .statusCode(404);
   }
 
+  @Test
+  void liveness_probe_is_available() {
+    given()
+        .when()
+            .get("/q/health/live")
+        .then()
+            .statusCode(200);
+  }
 }
