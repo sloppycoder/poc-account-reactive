84759727902520361c9a2e24fa1f6a76c7822ae6
Li Lin <guru.lin@gmail.com>
2021-06-11 18:43:43 +0800

    adding files missed in last commit.

--- src/main/java/org/vino9/poc/api/AccountsApi.java
+++ src/main/java/org/vino9/poc/api/AccountsApi.java
@@ -4,10 +4,7 @@ import io.smallrye.mutiny.Uni;
 import org.vino9.poc.model.AccountDetail;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
--- src/main/java/org/vino9/poc/api/AccountsApiService.java
+++ src/main/java/org/vino9/poc/api/AccountsApiService.java
@@ -1,22 +1,11 @@
 package org.vino9.poc.api;
 
 import io.smallrye.mutiny.Uni;
-import org.vino9.poc.api.*;
-import org.vino9.poc.model.*;
-
-
-import org.vino9.poc.model.AccountDetail;
-
-import java.util.List;
-import org.vino9.poc.api.NotFoundException;
-
-import java.io.InputStream;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaResteasyServerCodegen")
 public interface AccountsApiService {
-      Uni<Response> getAccountDetail(String id, SecurityContext securityContext)
-      throws NotFoundException;
+  Uni<Response> getAccountDetail(String id, SecurityContext securityContext);
 }
