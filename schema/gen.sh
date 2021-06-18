32967f81d7bb21944d64f90447fa5985c942375e
Li Lin <guru.lin@gmail.com>
2021-06-18 11:26:58 +0800

    refactor the code generation scheme.

--- schema/gen.sh
+++ schema/gen.sh
@@ -1,13 +1,22 @@
-#!/bin/bash
 
-# run generator
-../mvnw -f apigen.xml
+CLI_JAR=~/Downloads/swagger-codegen-cli-3.0.26.jar
 
-# there are some java files in src folder but we don't really need them.
-# one day we'll override the templates to solve these problems
+if [ ! -f CLI_JAR ]; then
+  rm -rf out
+  java -jar $CLI_JAR generate --config config.json --input-spec accounts-openapi.yaml -o out
+else
+  echo Cannot find $CLI_JAR file.
+fi
 
-# cleanup
-rm -f pom.xml
-rm -rf .openapi-generator
-rm -rf src
+if [ "$1" = "--copy" ]; then
+  echo
+  echo Copying generated files to src folder
+  echo
+	cp out/src/main/java/org/vino9/poc/model/*.java ../src/main/java/org/vino9/poc/model/.
+	cp out/src/main/java/org/vino9/poc/api/AccountsApi*.java ../src/main/java/org/vino9/poc/api/.
+	cp out/src/main/java/org/vino9/poc/api/impl/AccountsApiServiceImpl.java ../src/main/java/org/vino9/poc/api/impl/.
+fi
 
+echo
+echo check out directory for generated stuff
+echo do not forget to reformat all generated code
