04d50fbda224c4c45abf956e2f055342e56aaae7
Li Lin <guru.lin@gmail.com>
2021-06-19 22:14:08 +0800

    implemented MongoDB equivalent

--- src/main/java/org/vino9/poc/data/AccountDetailRepository.java
+++ src/main/java/org/vino9/poc/data/AccountDetailRepository.java
@@ -1,42 +1,52 @@
 package org.vino9.poc.data;
 
+import static com.mongodb.client.model.Filters.eq;
+
+import io.quarkus.mongodb.reactive.ReactiveMongoClient;
 import io.smallrye.mutiny.Uni;
-import io.vertx.mutiny.pgclient.PgPool;
-import io.vertx.mutiny.sqlclient.RowSet;
-import io.vertx.mutiny.sqlclient.Tuple;
+import java.security.SecureRandom;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import org.bson.conversions.Bson;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
-import org.vino9.poc.mapper.RowToModelMapper;
 import org.vino9.poc.model.AccountDetail;
 
 @ApplicationScoped
 public class AccountDetailRepository {
 
-    private static final String SQL_FIND_ACCOUNT_BY_ID =
-        "SELECT account_no, currency, country, branch_code, pg_sleep(delay) FROM accounts WHERE account_no = $1";
-
-    private static final String SQL_RANDOM_ACCOUNT =
-            "SELECT $1, currency, country, branch_code, pg_sleep(delay) "
-            + "FROM accounts OFFSET floor(random() * (SELECT COUNT(*)  FROM accounts)) LIMIT 1";
-
     @Inject
     Logger log;
 
     @Inject
-    PgPool client;
+    ReactiveMongoClient client;
+
+    @ConfigProperty(name = "app.database")
+    String database;
+
+    SecureRandom random = new SecureRandom();
 
     public Uni<AccountDetail> findByAccountNo(String accountNo) {
-        var sql =  "random".equalsIgnoreCase(accountNo)
-            ? SQL_RANDOM_ACCOUNT
-            : SQL_FIND_ACCOUNT_BY_ID;
-        return client
-            .preparedQuery(sql)
-            .execute(Tuple.of(accountNo))
-            .onItem().transform(RowSet::iterator)
-            .onItem().transform(
-                iterator -> iterator.hasNext()
-                    ? RowToModelMapper.rowToAccountDetail(iterator.next())
-                    : null);
+        Bson queryFilter;
+        if ("random".equalsIgnoreCase(accountNo)) {
+            //random.nextInt()
+            queryFilter = eq("id", 10);
+        } else {
+            queryFilter = eq("account_num", accountNo);
+        }
+
+        // randomly pick 1 of the first 10 docs
+        return client.getDatabase(database)
+            .getCollection("accounts")
+            .find(queryFilter)
+            .map(doc -> {
+                var account = new AccountDetail();
+                account.setAccountNo(doc.getString("account_num"));
+                account.setCurrency(doc.getString("currency"));
+                account.setCountry(doc.getString("country"));
+                account.setBranchCode(doc.getString("branch_code"));
+                return account;
+            })
+            .collect().first();
     }
 }
---
+++ src/main/resources/db/migration/V1__test_data.js
@@ -0,0 +1,13 @@
+db.accounts.insertMany([
+  {id:0, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000000", delay: 0.0},
+  {id:1, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000001", delay: 0.0},
+  {id:2, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000002", delay: 0.0},
+  {id:3, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000003", delay: 0.0},
+  {id:4, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000004", delay: 0.0},
+  {id:5, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000005", delay: 0.0},
+  {id:6, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000006", delay: 0.0},
+  {id:7, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000007", delay: 0.0},
+  {id:8, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000008", delay: 0.0},
+  {id:9, currency: "SGD", country: "SG", branch_code:"1111", account_num:"10000009", delay: 0.0},
+  {id:10, currency: "SGD", country: "SG", branch_code:"1111", account_num:"11223344", delay: 0.0}
+])
